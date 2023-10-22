367401c1f0e8a33f4e7b2e5c3040776413267d85
Zifan Gu <s219733@NucleusC084.cm.cluster>
2023-10-21 21:57:39 -0500

    add documentation on test files

--- tests/test_csgo.py
+++ tests/test_csgo.py
@@ -1,8 +1,8 @@
-import pytest
-from models import CSGO
-
 # add the following to command line before running pytest
 # export PYTHONPATH=/project/DPDS/Xiao_lab/shared/deep_learning_SW_RR/cell_segmentation/CSGO/src:$PYTHONPATH
+# see pytest.ini for CI configs
+
+from models import CSGO
 
 def test_csgo_init_no_gpu():
   cell_seg_go = CSGO()
