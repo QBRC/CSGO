9d5ec47b90c5c2d67e7aa8c41174e1f15e9a3916
Zifan Gu <s219733@NucleusC084.cm.cluster>
2023-10-22 13:24:34 -0500

    add resolution to mpp conversion

--- src/models.py
+++ src/models.py
@@ -1,11 +1,29 @@
 class CSGO():
-  def __init__(self, gpu=False, save=False):
+  def __init__(self, gpu=False, save=False, zoom=40, mpp=0.25):
+    """
+    Define model level info.
+    zoom: 1st data point to convert resolution and mpp
+    mpp: 2nd data point to convert resolution and mpp
+    standard equipment places 40x images at MPP = 0.25
+    """
     if gpu:
       # TODO: define device
       self.device = 0
     else:
       self.device = None
     self.save = save
+    self.zoom = zoom
+    self.mpp = mpp
+
+  def convert_resolution_to_mpp(self, img_resolution):
+    """
+    Converts the solution (e.g. 20x, 40x) to microns per pixel (MPP). Calculation based on previously defined zoom&mpp during class init.
+    e.g.: if 40x corresponds to 0.25 MPP, then 20x corresponds to 0.5 MPP
+    """
+    factor_from_defined_zoom = img_resolution / self.zoom
+    new_mpp = self.mpp / factor_from_defined_zoom
+    return new_mpp
+    
 
   def segment(self, img_path, cell_size = 50, img_resolution=20):
     # TODO: cell seg magic
--- tests/test_csgo.py
+++ tests/test_csgo.py
@@ -1,7 +1,7 @@
 # add the following to command line before running pytest
 # export PYTHONPATH=/project/DPDS/Xiao_lab/shared/deep_learning_SW_RR/cell_segmentation/CSGO/src:$PYTHONPATH
 # see pytest.ini for CI configs
-
+import pytest
 from models import CSGO
 
 def test_csgo_init_no_gpu():
@@ -11,3 +11,22 @@ def test_csgo_init_no_gpu():
 def test_csgo_init_with_gpu():
   cell_seg_go = CSGO(gpu=True)
   assert cell_seg_go.device == 0
+
+@pytest.fixture
+def csgo_for_mpp_check():
+  'create fixture to test multiple mpp conversion scenarios'
+  cell_seg_go = CSGO(zoom=40, mpp=0.25)
+  return cell_seg_go
+
+def test_resolution_mpp_convertion(csgo_for_mpp_check):
+  # integer conversions
+  new_mpp = csgo_for_mpp_check.convert_resolution_to_mpp(80)
+  assert new_mpp == 0.125
+
+  # float mpp check
+  new_mpp = csgo_for_mpp_check.convert_resolution_to_mpp(60)
+  expected_mpp = 0.25 / (60/40)
+  assert new_mpp == pytest.approx(expected_mpp)
+
+
+  
