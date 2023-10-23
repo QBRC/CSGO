702cb57d2a145f97cc3c795f79ff59149af16025
Zifan Gu <s219733@NucleusC084.cm.cluster>
2023-10-23 14:10:28 -0500

    set path relative to file location than pwd

--- src/hd_yolo.py
+++ src/hd_yolo.py
@@ -6,7 +6,9 @@ from run_patch_inference import main
 import argparse
 
 import os
-SRC_DIR = os.getcwd()
+
+# find the path this file
+SRC_DIR = os.path.realpath(os.path.dirname(__file__))
 
 class yolo_standalone():
   def __init__(self, img_path, device, mpp):
@@ -19,8 +21,8 @@ class yolo_standalone():
     args_yolo.data_path = self.img_path
     args_yolo.output_dir = './'
     args_yolo.device = self.device 
-    args_yolo.model = os.path.join(SRC_DIR, 'src/for_dev_only/pretrained_weights/lung_best.float16.torchscript.pt')
-    args_yolo.meta_info = os.path.join(SRC_DIR, 'src/hd_wsi/meta_info.yaml')
+    args_yolo.model = os.path.join(SRC_DIR, 'for_dev_only/pretrained_weights/lung_best.float16.torchscript.pt')
+    args_yolo.meta_info = os.path.join(SRC_DIR, 'hd_wsi/meta_info.yaml')
     args_yolo.mpp = self.mpp
     args_yolo.box_only = False
     args_yolo.export_text = False
@@ -31,5 +33,5 @@ class yolo_standalone():
 
   def run_inference(self):
     # 0 is the background (non-nuclei) pixel value 
-    nuclei_pred, patch = run_patch_inference.main(self.args_yolo)
+    nuclei_pred, patch = run_patch_inference.main(self.args_yolo, self.device)
     return nuclei_pred, patch
